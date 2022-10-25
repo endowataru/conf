0f14f971bca8630b215247101255f67a80d94bfe
Wataru Endo <4672809+endowataru@users.noreply.github.com>
2022-10-25 15:22:06 +0900

    Fix error of install-sheldon.bash in CI pipeline

--- install-sheldon.bash
+++ install-sheldon.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 CONF_DIR=~/.config/sheldon
-mkdir $CONF_DIR || true
+mkdir -p $CONF_DIR || true
 for f in plugins.toml; do
     ln -si $(cd $(dirname $0); pwd)/sheldon/$f $CONF_DIR/$f
 done
